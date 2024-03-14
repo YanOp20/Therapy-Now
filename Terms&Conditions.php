6100f039deb232f67fccacfd130b4694dbd70ecd
Your Name <you@example.com>
2024-03-14 11:00:30 +0300

    f

---
+++ Terms&Conditions.php
@@ -0,0 +1,59 @@
+<?php session_start(); ?>
+
+<?php include_once "head.php"; ?>
+
+<body>
+    <?php include_once "header.php"; ?>
+    <div class="term">
+        <div class="terms">
+            <h1>Terms and Conditions</h1>
+
+            <p>The Therapists are independent providers who are neither our employees nor agents nor representatives. The Platform's role is limited to enabling the Therapist Services. The Therapists themselves are responsible for the performance of the Therapist Services. If you feel the Therapist Services provided by the Therapist do not fit your needs or expectations, you may change to a different Therapist who provides services through the Platform. While we have a large database of Therapists, you should note that because of licensure requirements that differ from jurisdiction to jurisdiction, not all Therapists available on our database will be available for you to match with, or at any particular time, or for any set period of time. If a Therapist you have been connected with stops using the Platform at any time after you have been connected, we will send an email to notify you that your Therapist is no longer on the Platform and that you have the opportunity to match with a new Therapist.</p>
+
+            <p>While we hope the Therapist Services are beneficial to you, you understand, agree and acknowledge that they may not be the appropriate solution for everyone's needs and that they may not be appropriate for every particular situation and/or a substitute for certain mental health needs that might require in-person therapy services, such as active withdrawal from certain substances or anorexia nervosa.</p>
+
+            <p>IF YOU ARE THINKING ABOUT SUICIDE OR IF YOU ARE CONSIDERING HARMING YOURSELF OR OTHERS OR IF YOU FEEL THAT ANY OTHER PERSON MAY BE IN ANY DANGER OR IF YOU HAVE ANY MEDICAL EMERGENCY, YOU MUST IMMEDIATELY CALL YOUR LOCAL EMERGENCY SERVICES NUMBER AND NOTIFY THE RELEVANT AUTHORITIES (click here for a link to emergency and crisis resources in your area). THE PLATFORM IS NOT DESIGNED FOR USE IN ANY OF THE AFOREMENTIONED CASES AND THE THERAPISTS CANNOT PROVIDE THE ASSISTANCE REQUIRED IN ANY OF THE AFOREMENTIONED CASES.</p>
+
+            <p>THE PLATFORM IS NOT INTENDED FOR THE PROVISION OF CLINICAL DIAGNOSIS REQUIRING AN IN-PERSON EVALUATION AND YOU SHOULD NOT USE IT IF YOU NEED ANY OFFICIAL DOCUMENTATION OR APPROVALS FOR PURPOSES SUCH AS, BUT NOT LIMITED TO, COURT-ORDERED THERAPY OR EMOTIONAL SERVICE DOG CERTIFICATION. IT IS ALSO NOT INTENDED FOR ANY INFORMATION REGARDING WHICH DRUGS OR MEDICAL TREATMENT MAY BE APPROPRIATE FOR YOU, AND YOU SHOULD DISREGARD ANY SUCH ADVICE IF DELIVERED THROUGH THE PLATFORM.</p>
+
+            <p>DO NOT DISREGARD, AVOID, OR DELAY IN OBTAINING IN-PERSON CARE FROM YOUR DOCTOR OR OTHER QUALIFIED PROFESSIONAL BECAUSE OF INFORMATION OR ADVICE YOU RECEIVED THROUGH THE PLATFORM.</p>
+
+            <h2>Privacy and Security</h2>
+
+            <p>Protecting and safeguarding any information you provide through the Platform is extremely important to us. Information about our security and privacy practices can be found on our Privacy Policy available at betterhelp.com/privacy (The 'Privacy Policy').</p>
+
+            <p>BY AGREEING TO THIS AGREEMENT AND/OR BY USING THE PLATFORM, YOU ARE ALSO AGREEING TO THE TERMS OF THE PRIVACY POLICY. THE PRIVACY POLICY IS INCORPORATED INTO AND DEEMED A PART OF THIS AGREEMENT. THE SAME RULES THAT APPLY REGARDING CHANGES AND REVISIONS OF THIS AGREEMENT ALSO APPLY TO CHANGES AND REVISIONS OF THE PRIVACY POLICY.</p>
+
+            <h2>Third Party Content</h2>
+
+            <p>The Platform may contain other content, products or services which are offered or provided by third parties ("Third Party Content"), links to Third Party Content (including but not limited to links to other websites) or advertisements which are related to Third Party Content. We have no responsibility for the creation of any such Third Party Content, including (but not limited to) any related products, practices, terms or policies, and we will not be liable for any damage or loss caused by any Third Party Content.</p>
+
+            <h2>Disclaimer of Warranty and Limitation of Liability</h2>
+
+            <p>TO THE MAXIMUM EXTENT PERMITTED BY LAW, YOU HEREBY RELEASE US AND AGREE TO HOLD US HARMLESS FROM ANY AND ALL CAUSES OF ACTION AND CLAIMS OF ANY NATURE RESULTING FROM THE THERAPIST SERVICES OR THE PLATFORM, INCLUDING (WITHOUT LIMITATION) ANY ACT, OMISSION, OPINION, RESPONSE, ADVICE, SUGGESTION, INFORMATION AND/OR SERVICE OF ANY THERAPIST AND/OR ANY OTHER CONTENT OR INFORMATION ACCESSIBLE THROUGH THE PLATFORM.</p>
+
+            <p>YOU UNDERSTAND, AGREE AND ACKNOWLEDGE THAT THE PLATFORM IS PROVIDED "AS IS" WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES OF ANY KIND, INCLUDING BUT NOT LIMITED TO MERCHANTABILITY, NON-INFRINGEMENT, SECURITY, FITNESS FOR A PARTICULAR PURPOSE OR ACCURACY. THE USE OF THE PLATFORM IS AT YOUR OWN RISK. TO THE FULLEST EXTENT OF THE LAW, WE EXPRESSLY DISCLAIM ALL WARRANTIES OF ANY KIND, WHETHER EXPRESSED OR IMPLIED.</p>
+
+            <p>YOU UNDERSTAND, AGREE AND ACKNOWLEDGE THAT WE SHALL NOT BE LIABLE TO YOU OR TO ANY THIRD PARTY FOR ANY INDIRECT, INCIDENTAL, CONSEQUENTIAL, SPECIAL, PUNITIVE OR EXEMPLARY DAMAGES.</p>
+
+            <p>YOU UNDERSTAND, AGREE AND ACKNOWLEDGE THAT OUR AGGREGATE LIABILITY FOR DAMAGES ARISING WITH RESPECT TO THIS AGREEMENT AND ANY AND ALL USE OF THE PLATFORM WILL NOT EXCEED THE TOTAL AMOUNT OF MONEY PAID BY YOU OR ON YOUR BEHALF THROUGH THE PLATFORM IN THE 12 MONTHS PERIOD PRIOR TO THE DATE OF THE CLAIM.</p>
+
+            <p>If the applicable law does not allow the limitation of liability as set forth above, the limitation will be deemed modified solely to the extent necessary to comply with applicable law.</p>
+
+            <p>This section (limitation of liability) shall survive the termination or expiration of this Agreement.</p>
+
+            <h2>Arbitration</h2>
+
+            <p>THIS SECTION 6 OF THIS AGREEMENT SHALL BE REFERRED TO AS THE "ARBITRATION AGREEMENT."</p>
+
+            <p>By accepting the terms of this Arbitration Agreement, you and the Company (collectively, "Parties") agree that any dispute, claim, or controversy (except those specifically exempted below) arising out of or relating to (i) this Agreement and prior versions of this Agreement, or concerning the existence, applicability, breach, termination, enforcement, interpretation, scope, waiver, or validity thereof; and (ii) the use of the Platform or Therapist Services (collectively, "Disputes") will be resolved on an individual basis by final and binding arbitration, regardless of its date of accrual. Except as it otherwise provides, this Arbitration Agreement is intended to apply to the resolution of Disputes that otherwise would be resolved in a court of law or before a forum other than arbitration. All claims in arbitration are subject to the same statutes of limitation that would apply in court. By accepting the terms of this Arbitration Agreement, you and the Company mutually agree to waive their respective rights to trial by jury
+            </p>
+
+
+        </div>
+    </div>
+    <?php include_once "footer.php"; ?>
+
+</body>
+
+</html>
